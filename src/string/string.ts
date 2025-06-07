10ce5a38e5a42d29bcfebbaa2ab1be7c88b59d60
YuLiang Zhu <zhuyuliangm@outlook.com>
2025-06-07 06:47:59 +0000

    update

--- src/string/string.ts
+++ src/string/string.ts
@@ -3,20 +3,33 @@
  */
 
 /**
+ * First letter uppercase, other lowercase
+ * @category string
+ * @example
+ * ```
+ * capitalize('hello') => 'Hello'
+ * ```
+ */
+export function capitalize(str: string): string {
+  return str[0]?.toUpperCase() + str.slice(1).toLowerCase()
+}
+
+
+/**
  * Uppercases the first character in the `string`.
  * @param string - The string to uppercase the first character of.
  * @returns The string with the first character in uppercase.
  */
-export const ucFirst = (string: string) => {
-  if (typeof string !== 'string') {
+export const ucFirst = (str: string) => {
+  if (typeof str !== 'string') {
     return ''
   }
 
-  if (string.length === 0) {
-    return string
+  if (str.length === 0) {
+    return str
   }
 
-  return string[0]?.toUpperCase() + string.slice(1)
+  return str[0]?.toUpperCase() + str.slice(1)
 }
 
 /**
@@ -24,14 +37,23 @@ export const ucFirst = (string: string) => {
  * @param string - The string to lowercase the first character of.
  * @returns The string with the first character in lowercase.
  */
-export const lcFirst = (string: string) => {
-  if (typeof string !== 'string') {
+export const lcFirst = (str: string) => {
+  if (typeof str !== 'string') {
     return ''
   }
 
-  if (string.length === 0) {
-    return string
+  if (str.length === 0) {
+    return str
   }
 
-  return string[0]?.toLowerCase() + string.slice(1)
+  return str[0]?.toLowerCase() + str.slice(1)
+}
+
+/**
+ * Replace backslash to slash
+ *
+ * @category String
+ */
+export function slash(str: string) {
+  return str.replace(/\\/g, '/')
 }
